d8f495547c6f7cfcda49e88f5cfafde056d69cb7
crayola-eater <a-sheikh@outlook.com>
2021-04-07 14:34:06 +0100

    Created protected route

---
+++ src/auth/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+import React, { ComponentType } from 'react';
+import { Route } from 'react-router';
+import Loading from '../components/common/Loading/Loading';
+
+const ProtectedRoute: React.FC = ({ children, ...args }) => {
+  return (
+    <Route
+      component={withAuthenticationRequired(children as ComponentType, {
+        onRedirecting: () => <Loading />,
+      })}
+      {...args}
+    />
+  );
+};
+
+export default ProtectedRoute;
