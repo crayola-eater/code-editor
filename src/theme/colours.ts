6e1e9aa4f56855b3608da8413a1c8e4d17c0278b
crayola-eater <a-sheikh@outlook.com>
2021-04-07 14:16:09 +0100

    Created colours

---
+++ src/theme/colours.ts
@@ -0,0 +1,15 @@
+export const commonColours = {
+  white: '#ffffff',
+  black: '#000000',
+};
+
+export const appColours = {
+  background: commonColours.white,
+  font: commonColours.black,
+  primary: '#203040',
+};
+
+export const darkModeColours = {
+  font: commonColours.white,
+  background: '#232B32',
+};
