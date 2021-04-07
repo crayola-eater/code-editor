470f38bf1948e7dcc9a475e04bcc3f4690a96af0
crayola-eater <a-sheikh@outlook.com>
2021-04-07 12:05:33 +0100

    Configured Redux store

---
+++ src/store/store.ts
@@ -0,0 +1,22 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
+import persistStore from 'redux-persist/es/persistStore';
+import persistedReducer from './persistConfig';
+
+const ignoredActions = [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER];
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions,
+    },
+  }),
+  devTools: 'production' !== process.env.NODE_ENV,
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
