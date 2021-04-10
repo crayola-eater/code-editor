ae560765de0d324516766c46682ceb24568c0605
crayola-eater <a-sheikh@outlook.com>
2021-04-10 19:40:46 +0100

    Added test for selectFileViewerData selector

---
+++ src/store/selectors/selectFileViewerData/selectFileViewerData.test.ts
@@ -0,0 +1,64 @@
+import FileViewerStructure from '../../../types/FileViewerStructure';
+import { FilesState } from '../../reducers/files/reducer';
+import { RootState } from '../../store';
+import selectFileViewerData from './selectFileViewerData';
+
+it('should return a tree or should convert a list of user files to the file viewer structure', () => {
+  const userFiles: FilesState['userFiles'] = [
+    {
+      id: '1',
+      name: 'index1.js',
+      relativePath: 'test/index1.js',
+      code: 'console.log("Hello world");',
+      extension: 'js',
+    },
+    {
+      id: '2',
+      name: 'index2.js',
+      relativePath: 'test/subfolder/index2.js',
+      code: 'console.log("Hello world");',
+      extension: 'js',
+    },
+    {
+      id: '3',
+      name: 'index3.js',
+      relativePath: 'test/index3.js',
+      code: 'console.log("Hello world");',
+      extension: 'js',
+    },
+  ];
+
+  const state = {
+    files: { userFiles },
+  } as RootState;
+
+  const expectedResult: FileViewerStructure = {
+    id: expect.any(String),
+    name: 'test',
+    children: [
+      {
+        id: '1',
+        name: 'index1.js',
+        extension: 'js',
+      },
+      {
+        id: expect.any(String),
+        name: 'subfolder',
+        children: [
+          {
+            id: '2',
+            name: 'index2.js',
+            extension: 'js',
+          },
+        ],
+      },
+      {
+        id: '3',
+        name: 'index3.js',
+        extension: 'js',
+      },
+    ],
+  };
+
+  expect(selectFileViewerData(state)).toEqual(expectedResult);
+});
