cf57c81d54739068142f07e477835ccff4273e51
crayola-eater <a-sheikh@outlook.com>
2021-04-10 16:54:35 +0100

    Created SupportedExtensions type

---
+++ src/constants/supportedExtensions.ts
@@ -0,0 +1,16 @@
+type SupportedExtensions = {
+  JS: 'JAVASCRIPT';
+  JSX: 'JAVASCRIPT';
+  TS: 'TYPESCRIPT';
+  TSX: 'TYPESCRIPT';
+  PY: 'PY';
+  RB: 'RB';
+  JAVA: 'JAVA';
+  GO: 'GO';
+  HTML: 'HTML';
+  PHP: 'PHP';
+  CSS: 'CSS';
+  JSON: 'JSON';
+};
+
+export default SupportedExtensions;
