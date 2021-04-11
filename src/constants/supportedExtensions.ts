ce2685ee40590c7140ba3c6fd8660e98c1dcb3e7
crayola-eater <a-sheikh@outlook.com>
2021-04-11 20:07:06 +0100

    Changed supportedExtensions to object, exported type

--- src/constants/supportedExtensions.ts
+++ src/constants/supportedExtensions.ts
@@ -1,16 +1,18 @@
-type SupportedExtensions = {
-  JS: 'JAVASCRIPT';
-  JSX: 'JAVASCRIPT';
-  TS: 'TYPESCRIPT';
-  TSX: 'TYPESCRIPT';
-  PY: 'PY';
-  RB: 'RB';
-  JAVA: 'JAVA';
-  GO: 'GO';
-  HTML: 'HTML';
-  PHP: 'PHP';
-  CSS: 'CSS';
-  JSON: 'JSON';
+const supportedExtensions = {
+  JS: 'JAVASCRIPT',
+  JSX: 'JAVASCRIPT',
+  TS: 'TYPESCRIPT',
+  TSX: 'TYPESCRIPT',
+  PY: 'PY',
+  RB: 'RB',
+  JAVA: 'JAVA',
+  GO: 'GO',
+  HTML: 'HTML',
+  PHP: 'PHP',
+  CSS: 'CSS',
+  JSON: 'JSON',
 };
 
-export default SupportedExtensions;
+export type SupportedExtensions = typeof supportedExtensions;
+
+export default supportedExtensions;
