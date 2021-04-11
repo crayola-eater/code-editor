a68e8aea5deb82c058f7463fc7db629e7cbc94ff
crayola-eater <a-sheikh@outlook.com>
2021-04-11 20:17:57 +0100

    Created openFile thunk

---
+++ src/store/thunks/openFile/openFile.ts
@@ -0,0 +1,26 @@
+import { Dispatch } from 'redux';
+import supportedExtensions from '../../../constants/supportedExtensions';
+import FileViewerStructure from '../../../types/FileViewerStructure';
+import { addActiveFiles, setEditorActiveFile } from '../../reducers/files/reducer';
+import { RootState } from '../../store';
+
+const openFile = (node: FileViewerStructure) => {
+  return (dispatch: Dispatch, getState: () => RootState) => {
+    const { extension: fileExtension = '', id: fileId, children } = node;
+
+    if (children || !supportedExtensions.hasOwnProperty(fileExtension)) {
+      return;
+    }
+
+    const state = getState();
+    const activeFiles = state.files.activeFiles;
+
+    if (!activeFiles.includes(fileId)) {
+      dispatch(addActiveFiles(fileId));
+    }
+
+    dispatch(setEditorActiveFile(fileId));
+  };
+};
+
+export default openFile;
